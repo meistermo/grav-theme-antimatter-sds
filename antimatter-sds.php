0c1119660287a3f1ac6653ebb7711836b24d4e45
Moritz Menzel <moritz.menzel@mailbox.org>
2023-03-27 16:54:09 +0200

    Make db accessible to twig

--- antimatter-sds.php
+++ antimatter-sds.php
@@ -2,12 +2,13 @@
 namespace Grav\Theme;
 
 use Grav\Common\Theme;
+use Grav\Common\Grav;
 
 //Access plugin events in this class
-class AntimatterSDS extends Antimatter
-{
+class AntimatterSDS extends Antimatter {
     //Make db accessible to twig
     public function onTwigSiteVariables(): void {
         $this->grav["twig"]->twig_vars["db"] = Grav::instance()['database'];
-    }  
+    }      
 }
+
